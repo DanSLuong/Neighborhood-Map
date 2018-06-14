0e548a4978f1fee89e3840dfbf24004ba0e91eff
DanSLuong <hayate58@gmail.com>
2018-06-13 19:56:53 -0500

    Updated - Created markers and the showInfo function

--- js/script.js
+++ js/script.js
@@ -1,48 +1,75 @@
 // Map Variable.
 var map;
-
+var largeInfowindow;
 // Styles array to formate the map design
 var styles = [];
 
 var locations = [
-    { title: 'Googleplex', location: { lat: 37.4154126, lng: -122.0871518 } },
+    { title: 'Googleplex', location: { lat: 37.4220, lng: -122.0841 } },
     { title: 'NASA Ames Research Center', location: { lat: 37.4037799, lng: -122.1057486 } },
     { title: 'Computer History Museum', location: { lat: 37.4154126, lng: -122.0871518 } },
     { title: 'In-N-Out Burger', location: { lat: 37.4174795, lng: -122.0864403 } },
     { title: 'Shoreline Lake Boathouse', location: { lat: 37.4239477, lng: -122.112866 } }
 ];
 
-var marker = function (data) {
+
+var markers = function (data) {
+    var self = this;
+
     this.title = ko.observable(data.title);
-    this.location = ko.observableArray([data.location.lat, data.location.lng]);
+    this.location = ko.observable(data.location);
+    this.lat = ko.observable(data.lat);
+    this.lng = ko.observable(data.lng);
+    // Empty string observable to store the address in later
+    this.address = ko.observable('');
+
+    // Create Markers with the given data
+    var marker = new google.maps.Marker({
+        title: self.title(),
+        position: self.location(),
+        map: map
+    });
 };
 
-var viewModel = function () {
+
+var ViewModel = function () {
     var self = this;
+    
     this.locationsList = ko.observableArray([]);
 
-    // Add a marker for each location listed.
-    locations.forEach(function (locationItem) {
-        self.locationsList.push(new marker(locationItem));
+    locations.forEach(function (item) {
+        self.locationsList.push(new markers(item));
     });
 
-    this.currentLocation = ko.observable(this.locationsList()[0]);
+    this.query = ko.observable('');
 
-
+    
 };
 
-ko.applyBindings(new viewModel());
+var showInfo = function (marker) {
+    var self = this;
 
+    // Checks if old info window was open and closes it if it was
+    if (largeInfowindow) {
+        largeInfowindow.close();
+    }
+
+    // Basic info to show in infowindow for now
+    var info = '<div id="InfoWindowContent"></div>' + marker.title + '<br' + marker.location;
+    largeInfowindow = new google.maps.InfoWindow({ content: info });
+
+    // Loads the infowindow on the map at the marker
+    largeInfowindow.open(map, marker);
+};
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         // Mountain View California
         center: { lat: 37.4133865, lng: -122.1162864 },
         zoom: 13,
-        styles, styles, mapTypeControl: false
+        styles, styles,
+        mapTypeControl: false
     });
 
-    var largeInfowindow = new google.maps.InfoWindow();
-
-
-}
+    ko.applyBindings(new ViewModel());
+};
